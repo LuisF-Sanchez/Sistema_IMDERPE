5b8f07c44493af8929f0f85622a5fe700798dfa2
LuisF-Sanchez <nashronelite@gmail.com>
2026-07-30 23:36:19 -0400

    Delete controlador_ver_profesores.php

--- controlador_ver_profesores.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once 'conexion.php';
-
-$query = "SELECT id, nombre, apellido, cedula, telefono, correo, instituto_educativo FROM profesor_edu ORDER BY nombre ASC";
-$resultado = $conexion->query($query);
