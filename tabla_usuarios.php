1864d075c03150bc1b95b5a45e1404d123123362
LuisF-Sanchez <nashronelite@gmail.com>
2026-07-30 23:35:39 -0400

    Delete tabla_usuarios.php

--- tabla_usuarios.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once 'conexion.php';
-
-$sql = "SELECT id, nombre, cedula, telefono, correo, tipo 
-        FROM usuarios 
-        ORDER BY tipo ASC, nombre ASC";
-
-$resultado = $conexion->query($sql);
-?>
